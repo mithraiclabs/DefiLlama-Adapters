ee9e0f62f6f56d5aacaea2af2ccf76863baaa019
Tommy Johnson <tommy@psyoptions.io>
2022-08-17 22:28:14 -0400

    reconfigure to use fetch adapter

--- projects/psyoptions/index.js
+++ projects/psyoptions/index.js
@@ -1,15 +1,16 @@
-const { getAllTvlTokenMap } = require("@mithraic-labs/psy-metrics");
-const { getCoingeckoId, getProvider, } = require("../helper/solana");
+const { fetchURL } = require('../helper/utils')
+const { getCoingeckoId } = require("../helper/solana");
+
+const PSY_OPTIONS_TVL_ENDPOINT = "https://api.psyoptions.io/tvl";
 
 const mintToCoinGeckoIdMap = {
   "9tzZzEHsKnwFL1A3DyFJwj36KnZj3gZ7g4srWp9YTEoh": "arb-protocol"
 }
 
 async function tvl() {
-  const anchorProvider = getProvider();
   const getCg = await getCoingeckoId();
 
-  const mintAmountMap = await getAllTvlTokenMap(anchorProvider);
+  const mintAmountMap = await (await fetchURL(PSY_OPTIONS_TVL_ENDPOINT))?.data;
 
   const tvl = {};
 
